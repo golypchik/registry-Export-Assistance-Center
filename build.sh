fc0672abfa46e7460aaf4f749ee5ff9fa53a3df9
golypchik <kuzmichmaksim522@gmail.com>
2025-07-31 01:30:48 +0300

    Fix Python compatibility and update dependencies

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
 
-# Создаем суперпользователя через shell команду
+# Create superuser if it doesn't exist
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 import os
