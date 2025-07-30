5ec4d7dfe13ffb4206db636c572c4581786ebe13
golypchik <kuzmichmaksim522@gmail.com>
2025-07-31 02:17:28 +0300

    Configure for Render deployment with PostgreSQL

--- build.sh
+++ build.sh
@@ -2,14 +2,8 @@
 # exit on error
 set -o errexit
 
-# Upgrade pip
-python -m pip install --upgrade pip
-
-# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate
