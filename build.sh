5e41a54ecd2d19a1d1a385f75cda21c5cc2958d3
golypchik <kuzmichmaksim522@gmail.com>
2025-07-31 22:37:05 +0300

    Fix build script and settings for Render deployment

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-# File: c:\Users\User\Desktop\cert_checker\cert_checker\build.sh
-
-# Exit on error
+# exit on error
 set -o errexit
-
-# Upgrade pip
-python -m pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
